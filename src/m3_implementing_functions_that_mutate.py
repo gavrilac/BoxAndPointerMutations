6a4bc104454fdd094dc2dc106cc5e4d64c6288df
gavrilac <gavrilac@rose-hulman.edu>
2017-12-13 09:02:22 -0500

    Completed the TODOs

--- src/m3_implementing_functions_that_mutate.py
+++ src/m3_implementing_functions_that_mutate.py
@@ -134,7 +134,7 @@ def swap_colors(circle, rectangle):
       :type rectangle: rg.Rectangle
     """
     # ------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #          Tests have been written for you (above).
     #
     ####################################################################
@@ -145,7 +145,9 @@ def swap_colors(circle, rectangle):
     #      B = temp
     ####################################################################
     # ------------------------------------------------------------------
-
+    colorsave = circle.fill_color
+    circle.fill_color = rectangle.fill_color
+    rectangle.fill_color = colorsave
 
 # ----------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
